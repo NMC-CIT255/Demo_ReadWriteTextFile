8f12a1f1f7bcc237e5b1f54729b6a81ddc26e47f
John Velis <john@johnvelis.com>
2016-09-13 12:47:18 -0400

    Initial Commit

---
+++ Demo_ReadWriteTextFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_ReadWriteTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName;
+            int playerScore;
+
+            playerName = "Bonzo";
+            playerScore = 55;
+
+            WritePlayerHistory(playerName, playerScore);
+        }
+
+        private static void WritePlayerHistory(string playerName, int playerScore)
+        {
+
+        }
+    }
+}
