d9c0ff14c2d6fc0aacb2c551e4b20bfe034c1b2d
John Velis <john@johnvelis.com>
2016-09-14 14:44:43 -0400

    commit 244

--- Demo_ReadWriteTextFile/Program.cs
+++ Demo_ReadWriteTextFile/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Demo_ReadWriteTextFile
@@ -7,33 +9,74 @@ namespace Demo_ReadWriteTextFile
     {
         static void Main(string[] args)
         {
-            string playerName;
-            int playerScore;
+            List<HistoricScore> historicScores = new List<HistoricScore>();
+            historicScores = ReadPlayerHistory();
 
-            playerName = "Bonzo";
-            playerScore = 55;
+            DisplayHistoricScores(historicScores);
 
-            WritePlayerHistory("Bonzo", 56);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static void DisplayHistoricScores(List<HistoricScore> historicScores)
+        {
+            foreach (var historicScore in historicScores)
+            {
+                Console.WriteLine($"{historicScore.PlayerName} - {historicScore.PlayerScore}");
+            }
         }
 
         private static void WritePlayerHistory(string playerName, int playerScore)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                StringBuilder sb = new StringBuilder();
-                sb.Clear();
-                sb.Append(playerName);
-                sb.Append(",");
-                sb.Append(playerScore++);
 
-                using (StreamWriter sw = new StreamWriter("Data//PlayerHistory.txt", true))
-                {
-                    sw.WriteLine(sb.ToString());
-                }
-            }
         }
 
 
-        private static void ReadPlayerHistory()
+        private static List<HistoricScore> ReadPlayerHistory()
+        {
+            const char delineator = ','; // delineator in a CSV file
+
+            List<HistoricScore> historicScores = new List<HistoricScore>();
+
+            //
+            // create lists to hold the historic score strings
+            //
+            List<string> historicScoresStringList = new List<string>();
+
+            //
+            // initialize a StreamReader object for reading
+            //
+            StreamReader sReader = new StreamReader(DataSettings.dataFilePath);
+
+            //
+            // read all data from the data file
+            //
+            using (sReader)
+            {
+                // keep reading lines of text until the end of the file is reached
+                while (!sReader.EndOfStream)
+                {
+                    historicScoresStringList.Add(sReader.ReadLine());
+                }
+            }
+
+            //
+            // separate lines into fields and build out the list of historic scores
+            //
+            foreach (string historicScore in historicScoresStringList)
+            {
+                //
+                // use the Split method and the delineator on the array to separate each property into an array of properties
+                //
+                string[] fields = historicScore.Split(delineator);
+
+                //
+                // populate the historic scores list with HistoricScore objects
+                //
+                historicScores.Add(new HistoricScore() { PlayerName = fields[0], PlayerScore = Convert.ToInt32(fields[1]) });
+            }
+
+            return historicScores;
+        }
     }
 }
